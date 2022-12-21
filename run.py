bb4b3c210d2b78ddee4197a19c0ab7f372a19c86
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2022-12-21 22:29:25 +0300

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-from ReBook import app
-
-if __name__ == '__main__':
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-    app.run(host="0.0.0.0", port="5001", debug=True)
-
-
