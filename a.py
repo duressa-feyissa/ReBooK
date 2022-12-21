020dcc3991fc529d578c06778fcae8def8f4041e
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2022-12-21 22:30:09 +0300

    Delete a.py

--- a.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-email_address = "duresafeyis2022@gmail.com"
-
-response = requests.get(
-	"https://isitarealemail.com/api/email/validate", 
-	params={'email': email_address})
-status = response.json()['status']
-print(status)
